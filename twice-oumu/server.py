b81db827c15725c07c90f3ccf21d2f86ad978114
Osamu Kurihara <oskuri@jp.ibm.com>
2018-02-12 21:45:59 +0900

    translate

--- twice-oumu/server.py
+++ twice-oumu/server.py
@@ -1,6 +1,5 @@
 from cloudant import Cloudant
-from flask import Flask, render_template, request, jsonify, abort
-import cf_deployment_tracker
+from flask import Flask, request, abort
 
 from linebot import (
     LineBotApi, WebhookHandler
@@ -9,11 +8,13 @@ from linebot.exceptions import (
     InvalidSignatureError
 )
 from linebot.models import (
-    MessageEvent, TextMessage, TextSendMessage
+    MessageEvent, TextMessage, TextSendMessage,
 )
 
-from os.path import join, dirname
+import json
+from watson_developer_cloud import LanguageTranslatorV2 as LanguageTranslator
 import os
+import urllib.request, urllib.parse
 
 app = Flask(__name__)
 
@@ -23,6 +24,11 @@ port = int(os.getenv('PORT', 8080))
 
 ACCESSTOKEN = os.environ['ACCESSTOKEN']
 CHANNELSECRET = os.environ['CHANNELSECRET']
+
+VCAP_SERVICES = json.loads(os.environ['VCAP_SERVICES'])
+
+LT_NAME = VCAP_SERVICES['language_translator'][0]['credentials']['username']
+LT_PASS = VCAP_SERVICES['language_translator'][0]['credentials']['password']
 
 line_bot_api = LineBotApi(os.environ['ACCESSTOKEN'])
 handler = WebhookHandler(os.environ['CHANNELSECRET'])
@@ -46,7 +52,13 @@ def callback():
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    reply = event.message.text + event.message.text
+    language_translator = LanguageTranslator(
+        username=LT_NAME,password=LT_PASS)
+    translation = language_translator.translate(
+        text=event.message.text,
+        source='ja',
+        target='en')
+    reply = json.dumps(translation, indent=2, ensure_ascii=False).replace("\"", "")
     line_bot_api.reply_message(
         event.reply_token,
         TextSendMessage(text=reply))
